f77e6f985f1800e742f3c959d58163d771eaa3de
Anastasia Nevodchikova <anevodchikova@MacBook-Pro-Anastasia.local>
2022-02-03 21:26:07 +0300

    add ex02 for CPP02

--- CPP_Module_02/ex02/Fixed.cpp
+++ CPP_Module_02/ex02/Fixed.cpp
@@ -56,8 +56,120 @@ float Fixed::toFloat(void) const{
 	return (static_cast<float>(value) / (1 << bits));
 }
 
+Fixed& Fixed::operator++()
+{
+     this->setRawBits(this->getRawBits() + 1);
+ 
+    return *this;
+}
+ 
+Fixed& Fixed::operator--()
+{
+    this->setRawBits(this->getRawBits() - 1);
+ 
+    return *this;
+}
+ 
+Fixed Fixed::operator++(int)
+{
+	Fixed temp(this->getRawBits());
+ 
+     ++(*this); 
+    return temp;
+}
+ 
+Fixed Fixed::operator--(int)
+{
+    Fixed temp(this->getRawBits());
+     --(*this); 
+
+    return temp; 
+}
+
 std::ostream & operator<<(std::ostream &os, Fixed const& f)
 {
 	os << f.toFloat();
 	return (os);
+}
+
+bool operator> (const Fixed &f1, const Fixed &f2)
+{
+    return f1.toFloat() > f2.toFloat();
+}
+
+bool operator< (const Fixed &f1, const Fixed &f2)
+{
+     return f1.toFloat() < f2.toFloat();
+}
+ 
+bool operator>= (const Fixed &f1, const Fixed &f2)
+{
+    return f1 < f2;
+}
+ 
+bool operator<= (const Fixed &f1, const Fixed &f2)
+{
+    return f1 > f2;
+}
+
+bool operator== (const Fixed &f1, const Fixed &f2)
+{
+    return (f1.toFloat() == f2.toFloat());
+}
+ 
+bool operator!= (const Fixed &f1, const Fixed &f2)
+{
+    return !(f1== f2);
+}
+
+Fixed operator+(const Fixed &f1, const Fixed &f2)
+{
+	return Fixed(f1.toFloat() + f2.toFloat());
+}
+
+Fixed operator-(const Fixed &f1, const Fixed &f2)
+{
+	return Fixed(f1.toFloat() - f2.toFloat());
+}
+
+Fixed operator*(const Fixed &f1, const Fixed &f2)
+{
+	return Fixed(f1.toFloat() * f2.toFloat());
+}
+
+Fixed operator/(const Fixed &f1, const Fixed &f2)
+{
+	return Fixed(f1.toFloat() / f2.toFloat());
+}
+
+const Fixed & Fixed::min(const Fixed & f1, const Fixed & f2)
+{
+	if (f1 > f2)
+		return (f2);
+	else
+		return (f1);
+}
+
+const Fixed & Fixed::max(const Fixed & f1, const Fixed & f2)
+{
+	if (f1 > f2)
+		return (f1);
+	else
+		return (f2);
+}
+
+Fixed & Fixed::min(Fixed & f1, Fixed & f2)
+{
+	if (f1 > f2)
+		return (f2);
+	else
+		return (f1);
+}
+
+Fixed & Fixed::max(Fixed & f1, Fixed & f2)
+{
+	if (f1 > f2)
+		return (f1);
+	else
+		return (f2);
 }
--- CPP_Module_02/ex02/Fixed.hpp
+++ CPP_Module_02/ex02/Fixed.hpp
@@ -17,6 +17,24 @@ public:
 	void setRawBits( int const raw );
 	int toInt(void) const;
 	float toFloat(void) const;
+	Fixed& operator++(); // версия префикс
+    Fixed& operator--(); // версия префикс
+    Fixed operator++(int); // версия постфикс
+    Fixed operator--(int);
+	static const Fixed & min(const Fixed & f1, const Fixed & f2);
+	static const Fixed & max(const Fixed & f1, const Fixed & f2);
+	static Fixed & min( Fixed & f1,  Fixed & f2 );
+	static Fixed & max( Fixed & f1,  Fixed & f2 );
 };
 
 std::ostream & operator<<(std::ostream &os, Fixed const& f);
+bool operator> (const Fixed &f1, const Fixed &f2);
+bool operator<= (const Fixed &f1, const Fixed &f2);
+bool operator< (const Fixed &f1, const Fixed &f2);
+bool operator>= (const Fixed &f1, const Fixed &f2);
+bool operator== (const Fixed &f1, const Fixed &f2);
+bool operator!= (const Fixed &f1, const Fixed &f2);
+Fixed operator+(const Fixed &f1, const Fixed &f2);
+Fixed operator-(const Fixed &f1, const Fixed &f2);
+Fixed operator*(const Fixed &f1, const Fixed &f2);
+Fixed operator/(const Fixed &f1, const Fixed &f2);
