6bb7ed268c058a58f5d3abf6e37c7f56251df273
troxanna <anevodchikova29@gmail.com>
2022-04-15 14:59:02 +0300

    add main

--- CPP_Module_05/ex01/main.cpp
+++ CPP_Module_05/ex01/main.cpp
@@ -2,17 +2,77 @@
 
 int main()
 {
-    try
-	{
-		Bureaucrat bibo("BIBO", 1);
-		Bureaucrat biba("BIBA", 150);
-		//Bureaucrat bibe("BIBE", 1000);
-		Bureaucrat bibu("BIBU", -1000);
+
+	try {
+		Form	test("Form1", 1, 1);
+		std::cout << "1: OK" << std::endl << test << std::endl;
+	} catch (std::exception & e) {
+		std::cout << "1: " << e.what() << std::endl;
 	}
-	catch (std::exception & e)
-	{
-		std::cout << "LOX" << std::endl;
-		std::cout << e.what() << std::endl;
+
+	std::cout << std::endl;
+
+	try {
+		Form	test("Form2", 150, 150);
+		std::cout << "2: OK: " << std::endl << test << std::endl;
+	} catch (std::exception & e) {
+		std::cout << "2: " << e.what() << std::endl;
 	}
-    return 0;
+
+	try {
+		Form	test("Form3", 151, 150);
+		std::cout << "3: OK: " << std::endl << test << std::endl;
+	} catch (std::exception & e) {
+		std::cout << "3: " << e.what() << ": sign Grade - 151" << std::endl;
+	}
+
+	std::cout << std::endl;
+
+	try {
+		Form	test("Form4", 1, 0);
+		std::cout << "4: OK" << std::endl << test << std::endl;
+	} catch (std::exception & e) {
+		std::cout << "4: " << e.what() << ": execute Grade - 0" << std::endl;
+	}
+
+	try {
+		Form	testF("Form5", 1, 1);
+		std::cout << testF << std::endl;
+		Bureaucrat	testB("bro", 150);
+		std::cout << testB << std::endl;
+		std::cout << "trying to sign" << std::endl;
+		testB.signForm(&testF);
+		std::cout << "5: OK" << ": form signed " << testF.getName() << std::endl;
+	} catch (std::exception & e) {
+		std::cout << "5: " << e.what() << ": form can't be signed" << std::endl;
+	}
+
+	std::cout << std::endl;
+
+	try {
+		Form	testF("Form6", 1, 1);
+		std::cout << testF << std::endl;
+		Bureaucrat	testB("anton", 1);
+		std::cout << testB << std::endl;
+		testB.signForm(&testF);
+		std::cout << "6: OK" << ": form signed " << testF.getName() << std::endl;
+	} catch (std::exception & e) {
+		std::cout << "6: " << e.what() << ": form can't be signed" << std::endl;
+	}
+
+
+	std::cout << std::endl;
+
+	try {
+		Form	testF("Form7", 100, 1);
+		std::cout << testF << std::endl;
+		Bureaucrat	testB("egor", 99);
+		std::cout << testB << std::endl;
+		std::cout << "trying to sign" << std::endl;
+		testB.signForm(&testF);
+		std::cout << "7" << ": form signed " << testF.getName() << std::endl;
+	} catch (std::exception & e) {
+		std::cout << "7: " << e.what() << ": form can't be signed" << std::endl;
+	}
+	return (0);
 }
--- CPP_Module_05/ex02/main.cpp
+++ CPP_Module_05/ex02/main.cpp
@@ -1,18 +1,37 @@
 #include "Bureaucrat.hpp"
+#include "Form.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "ShrubberyCreationForm.hpp"
 
 int main()
 {
-    try
-	{
-		Bureaucrat bibo("BIBO", 1);
-		Bureaucrat biba("BIBA", 150);
-		//Bureaucrat bibe("BIBE", 1000);
-		Bureaucrat bibu("BIBU", -1000);
-	}
-	catch (std::exception & e)
-	{
-		std::cout << "LOX" << std::endl;
-		std::cout << e.what() << std::endl;
-	}
-    return 0;
+    Bureaucrat a("Anton", 1);
+	Bureaucrat e("Egor", 150);
+
+	try {Bureaucrat b("Bro", -1000);}
+	catch(std::exception & e) {std::cout << e.what() << std::endl;}
+
+	std::cout << std::endl << std::endl;
+	ShrubberyCreationForm test("dom");
+	test.beSigned(e);
+	try {test.execute(a);}
+	catch(std::exception & e) {std::cout << e.what() << std::endl;}
+
+	std::cout << std::endl << std::endl;
+	PresidentialPardonForm test1("Shrek");
+	try {test1.beSigned(a);}
+	catch(std::exception & e) {std::cout << e.what() << std::endl;}
+	test1.beSigned(e);
+	try {test1.execute(a);}
+	catch(std::exception & e) {std::cout << e.what() << std::endl;}
+	try {test1.execute(a);}
+	catch(std::exception & e) {std::cout << e.what() << std::endl;}
+
+	std::cout << std::endl << std::endl;
+	RobotomyRequestForm test2("Random");
+	try {test2.beSigned(a);}
+	catch(std::exception & e) {std::cout << e.what() << std::endl;}
+	try {test2.execute(a);}
+	catch(std::exception & e) {std::cout << e.what() << std::endl;}
 }
